e4aa7490a1efc1d6a74b5a26f5e6c4dee83fd103
MazidulIslam <56502659+MazidulIslam@users.noreply.github.com>
2021-10-31 01:39:01 +0600

    get offers, post offers, manage order

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+let ObjectId = require("mongodb").ObjectID;
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const port = process.env.PORT || 5000;
@@ -20,10 +21,64 @@ async function run() {
     await client.connect();
     console.log("connected to db");
     const database = client.db("exploreWonderland");
-    const placeCollections = database.collection("places");
-    // Query for a movie that has the title 'Back to the Future'
+    const offerCollections = database.collection("offers");
+    const imageCollections = database.collection("gallery");
+    const orderCollections = database.collection("orders");
+
+    // GET API
+    app.get("/offers", async (req, res) => {
+      console.log(req.body);
+      const cursor = offerCollections.find({});
+      const offers = await cursor.toArray();
+      res.send(offers);
+    });
+    // GET Single API Offer
+    app.get("/offers/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("hitting id", id);
+      const query = { _id: ObjectId(id) };
+      const offer = await offerCollections.findOne(query);
+      res.json(offer);
+    });
+    //   GET All Orders
+    app.get("/orders", async (req, res) => {
+      const cursor = orderCollections.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+    // GET Single API Order
+    app.get("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("hitting id", id);
+      const query = { _id: ObjectId(id) };
+      const order = await orderCollections.findOne(query);
+      res.json(order);
+    });
+    // POST  API
+    app.post("/offers", async (req, res) => {
+      const offer = req.body;
+      console.log("hit post api", offer);
+      const result = await offerCollections.insertOne(offer);
+      res.json(result);
+    });
+
+    // POST ORDERS
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      console.log("hit post api", order);
+      const result = await orderCollections.insertOne(order);
+      res.json(result);
+    });
+
+    // DELETE API
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollections.deleteOne(query);
+      res.json(result);
+      // on module 65.8
+    });
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
